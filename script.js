be9a0ec2fc75389ae19ac1f8e702e36da0fe35df
Macnelson9 <michaelofatu@gmail.com>
2025-04-17 15:36:12 +0100

    Refactor loader and navigation components; enhance styles and functionality

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const mainMenuBtn = document.getElementById('main-menu-btn');
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+const loaderContainer = document.querySelector('.loader-container');
+// const image1 = document.querySelector('.image-1');
+// const image2 = document.querySelector('.image-2');
+// const image3 = document.querySelector('.image-3');
+// const image4 = document.querySelector('.image-4');
+
+window.addEventListener('DOMContentLoaded', function () {
+    loaderContainer.style.display = 'none';
+})
+
+function toggleMenu() {
+    mobileNav.classList.toggle('active');
+    mainMenuBtn.classList.toggle('active');
+}
+
+mainMenuBtn.addEventListener('click', toggleMenu);
+mobileMenuBtn.addEventListener('click', toggleMenu);
+
+// gsap.from(".image-1", { // selector text, Array, or object
+//     x: '120%', 
+//     duration: 2,
+//     rotate: 10,
+//     ease: "power2.inOut",
+//   });
